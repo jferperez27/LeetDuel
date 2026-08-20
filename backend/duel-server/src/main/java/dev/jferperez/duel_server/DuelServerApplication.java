c2632212e6e046d0bf984f84a0ddc7dcae87f6c7
jferperez27 <juan_j_fernandez@brown.edu>
2026-08-20 00:34:34 -0500

    FCA-2: add Spring Boot framework

---
+++ backend/duel-server/src/main/java/dev/jferperez/duel_server/DuelServerApplication.java
@@ -0,0 +1,13 @@
+package dev.jferperez.duel_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DuelServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DuelServerApplication.class, args);
+	}
+
+}
---
+++ backend/duel-server/src/test/java/dev/jferperez/duel_server/DuelServerApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.jferperez.duel_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DuelServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
